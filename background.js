2029f5f3ec1cf77a3ed3daa142767d38642a12d4
meganstone1 <b9019760@ncl.ac.uk>
2023-04-04 20:04:53 +0100

    implemented haveibeenpwned api to show number of times breached

--- background.js
+++
@@ -1,6 +0,0 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.action === "keyup") {
-        var userInput = request.value;
-        chrome.tabs.sendMessage(sender.tab.id, {action: "display", value: userInput});
-    }
-});
